f0ac68c0c8f7ae44cb0b156c94ce069c2b5ac314
MaksimS <sipatovgt@mail.ru>
2022-11-07 22:46:27 +0400

    ex01

---
+++ ex01/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+int GetDigits(int number){
+    int count = 0;
+    while(number > 0){
+        count = number / 10;
+        count++;
+    }
+    return count;
+}
+
+GetDigits(number);
+Console.WriteLine("Ваш результат: " + count);
---
+++ ex02/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24 
+// 5 -> 120
+
+Console.WriteLine("Введите число!");
+int number = int.Parse(Console.ReadLine()!);
+int FactorialDigit(int number){
+    int count = 1;
+    int result = 1;
+    while(count <= number){
+    result = result * count;
+    count++;
+    }
+    return result;
+}
+int N = FactorialDigit(number);
+
+Console.WriteLine("Ваш результат: " + N);
