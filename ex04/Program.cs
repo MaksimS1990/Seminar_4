8addb97626ddf8f9222ec04750bc867b787c3062
MaksimS <sipatovGT@mail.ru>
2022-11-09 10:41:26 +0400

    ex04

---
+++ ex04/Program.cs
@@ -0,0 +1,21 @@
+﻿// напишите программу, которая принимает на вход число(A) и выдаёт сумму чисел от 1 до A.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+Console.WriteLine("Введите Ваше число ");
+int number = int.Parse(Console.ReadLine()!);
+
+int GetSumDigitsA (int number){
+    int result = 0;   //переменаая, в которую складываем результат
+    int count = 0;    //переменая-счетчик
+
+    while(count < number){
+        count++;
+        result = result + count;
+    }
+    return result;
+}
+
+int res = GetSumDigitsA(number);
+Console.WriteLine("Ваш результат равен: " + res);
